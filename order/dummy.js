490f150488fb0798ae76b1883403beae1eb5ced2
JSH99 <tjgus9966@naver.com>
2023-05-13 21:08:55 +0900

    주문 내역 검색 page

---
+++ order/dummy.js
@@ -0,0 +1,167 @@
+// dummy data
+export const data = [
+    {
+        id: 1,
+        phone_number: '010-1234-5678',
+        menues : [
+            {
+                name : "어묵탕",
+                quantity : 1
+            },
+            {
+                name : "탕탕탕",
+                quantity : 2
+            }],
+        table_num: 8,
+        created_at: '05-13 23:21:55',
+        status: 'in_progress'
+    },
+    {
+        id: 2,
+        phone_number: '011-1234-5678',
+        menues : [
+            {
+                name : "어묵탕탕",
+                quantity : 1
+            },
+            {
+                name : "컵라면",
+                quantity : 2
+            }],
+        table_num: 10,
+        created_at: '05-13 20:21:55',
+        status: 'done'
+    },
+    {
+        id: 3,
+        phone_number: '010-1234-5678',
+        menues : [
+            {
+                name : "햄계란후라이",
+                quantity : 333
+            }],
+        table_num: 8,
+        created_at: '05-13 20:21:55',
+        status: 'done'
+    },
+    {
+        id: 4,
+        phone_number: '011-1234-5678',
+        menues : [
+            {
+                name : "코카콜라 맛있다",
+                quantity : 10
+            },
+            {
+                name : "맛있으면 또 먹어",
+                quantity : 100
+            },
+            {
+                name : "또 먹으면 배탈나",
+                quantity : 1000
+            }],
+        table_num: 11,
+        created_at: '05-13 20:21:55',
+        status: 'in_progress'
+    },
+    {
+        id: 5,
+        phone_number: '010-1234-3333',
+        menues : [
+            {
+                name : "멋사",
+                quantity : 0
+            },
+            {
+                name : "이니로",
+                quantity : 0
+            },
+            {
+                name : "유엠씨",
+                quantity : 0
+            }],
+        table_num: 1,
+        created_at: '05-13 20:21:55',
+        status: 'in_progress'
+    },
+    {
+        id: 6,
+        phone_number: '010-1234-1234',
+        menues : [
+            {
+                name : "계란탕",
+                quantity : 1
+            },
+            {
+                name : "맛있어",
+                quantity : 2
+            }],
+        table_num: 5,
+        created_at: '05-13 22:21:55',
+        status: 'in_progress'
+    },
+    {
+        id: 7,
+        phone_number: '010-1234-1234',
+        menues : [
+            {
+                name : "소세지",
+                quantity : 1100
+            },
+            {
+                name : "황도",
+                quantity : 2
+            }],
+        table_num: 100,
+        created_at: '05-13 21:41:55',
+        status: 'in_progress'
+    },
+    {
+        id: 8,
+        phone_number: '010-1234-1234',
+        menues : [
+            {
+                name : "어묵탕",
+                quantity : 1
+            },
+            {
+                name : "컵라면",
+                quantity : 2
+            }],
+        table_num: 20,
+        created_at: '05-13 19:21:55',
+        status: 'in_progress'
+    },
+    {
+        id: 9,
+        phone_number: '010-1234-1234',
+        menues : [
+            {
+                name : "나쵸",
+                quantity : 3
+            }],
+        table_num: 55,
+        created_at: '05-13 09:21:55',
+        status: 'done'
+    },
+    {
+        id: 10,
+        phone_number: '010-1234-1234',
+        menues : [
+            {
+                name : "금도끼",
+                quantity : 1
+            },
+            {
+                name : "은도끼",
+                quantity : 2
+            },
+            {
+                name : "동도끼",
+                quantity : 3
+            }],
+        table_num: 0,
+        created_at: '05-13 09:21:55',
+        status: 'done'
+    },
+  ];
