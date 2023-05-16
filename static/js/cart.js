6aa5267bf1d86d5608086e071d70d313b4683f34
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-05-17 00:39:07 +0900

    Update cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -52,10 +52,14 @@ function order_yes(){
    localStorage.setItem('order',JSON.stringify(order))
     localStorage.setItem('table_number',document.getElementById('table_number').value);
     localStorage.setItem('phone_number',document.getElementById('phone_number').value);
-    var newWindow = window.open("http://kko.to/hD0-Cb93jJ", "_blank");
-    setTimeout(function(){
-        newWindow.close();
-    }, 1000);
+//    var newWindow = window.open("http://kko.to/hD0-Cb93jJ", "_blank");
+//    setTimeout(function(){
+//        newWindow.close();
+//        console.log(1)
+//    }, 1000);
+    var xhr = new XMLHttpRequest();
+      xhr.open('GET', "http://kko.to/hD0-Cb93jJ", true);
+      xhr.send();
     location.replace("/order_complete")
 }
 
